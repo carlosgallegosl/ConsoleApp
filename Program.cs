b4509c6b79fcb1b1a5d55d3ed9b05e262f615a7b
Juan Carlos <juan.gallegos@bachoco.net>
2019-02-04 17:45:19 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 20;
+            Console.WriteLine("Hola a todos!");
+            Console.WriteLine("Maestria Tecnologias Web y Dispositivos moviles");
+        }
+    }
+}
